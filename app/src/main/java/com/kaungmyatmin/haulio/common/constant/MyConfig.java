195132b0031f6fa9d49206b81a52444073b6cbf9
Kaung Myat Min <kaungmyatmin.biz@gmail.com>
2020-01-21 20:51:07 +0630

    change map zoom

--- app/src/main/java/com/kaungmyatmin/haulio/common/constant/MyConfig.java
+++ app/src/main/java/com/kaungmyatmin/haulio/common/constant/MyConfig.java
@@ -3,5 +3,5 @@ package com.kaungmyatmin.haulio.common.constant;
 interface MyConfig {
     int SPLASH_DURATION_MILLISECOND = 1000;
     boolean IS_PRODUCTION = false;
-    int MAP_ZOOM_SIZE = 2;
+    int MAP_ZOOM_SIZE = 12;
 }
